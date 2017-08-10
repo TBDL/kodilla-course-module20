f079909dd63d4ea7ffd7e229ecbdae296bff3540
Tomasz Badelek <tbadelek@gmail.com>
2017-08-10 22:11:53 +0200

    1st

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,34 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
 
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+       SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+       String result = simpleUser.getUsername();
+
+       if(result.equals("theForumUser")){
+           System.out.println("test OK");
+       } else {
+           System.out.println("Error!");
+       }
+
+       // Quest 6.2
+       Calculator calculator = new Calculator(10,5);
+
+       int addResult = calculator.add(10,5);
+       int substractResult = calculator.substract(10,5);
+
+       if(addResult == (calculator.getA() + calculator.getB()) &&
+          substractResult == (calculator.getA() - calculator.getB())){
+
+           System.out.println("test OK");
+        } else {
+           System.out.println("Error!");
+    }
+
     }
 }
