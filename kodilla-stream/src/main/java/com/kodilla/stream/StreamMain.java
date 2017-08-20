d4202d457c023e5d3ac957f777b7315f984b7e3d
Tomasz Badelek <tbadelek@gmail.com>
2017-08-20 21:12:30 +0200

    7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,17 +1,14 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 import static jdk.nashorn.internal.objects.NativeString.toUpperCase;
 
 public class StreamMain {
     public static void main(String[] args) {
 
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-        poemBeautifier.beautify("apples", text -> "ABC " + text + " ABC");
-        poemBeautifier.beautify("apples", text -> toUpperCase(text));
-        poemBeautifier.beautify("apples", text -> new StringBuilder(text).reverse().toString());
-        poemBeautifier.beautify("apples", text -> "\u001B[31m" + text);
-
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
