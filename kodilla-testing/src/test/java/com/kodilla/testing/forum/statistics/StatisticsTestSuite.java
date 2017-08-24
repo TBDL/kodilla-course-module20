e1c4434338618b0168c02fee29b46e8ae58b534e
Tomasz Badelek <tbadelek@gmail.com>
2017-08-24 11:48:42 +0200

    6.5 final

--- kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/StatisticsTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/StatisticsTestSuite.java
@@ -101,8 +101,20 @@ public class StatisticsTestSuite {
 
     }
 
+    /*
+        Map<Integer, Integer> statisticsList = new HashMap<Integer, Integer>();
+        statisticsList.put(0,usersAmount);
+        statisticsList.put(1,postsAmount);
+        statisticsList.put(2,commentsAmount);
+        statisticsList.put(3,averagePostAmountPerUser);
+        statisticsList.put(4,averageCommentsAmountPerUser);
+        statisticsList.put(5,averageCommentsAmountPerPost);
+     */
+
+
+
     @Test
-        public void testCalculateStatistics() {
+        public void testCalculateStatisticsUsersAmount() {
         //Given
         Statistics statisticsMock = mock(Statistics.class);
         int postsAmountTest = 1000;
@@ -123,17 +135,137 @@ public class StatisticsTestSuite {
         Assert.assertEquals(Integer.valueOf(100), wynik.get(0));
     }
 
+    @Test
+    public void testCalculateStatisticsPostsAmount() {
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+        int postsAmountTest = 1000;
+        int commentsAmountTest = 0;
+        ArrayList<String> userList = new ArrayList<String>();
+        for (int i = 0; i < 100; i++) {userList.add("user");}
 
-    /*
-         //When
+        when(statisticsMock.postsCount()).thenReturn(postsAmountTest);
+        when(statisticsMock.commentsCount()).thenReturn(commentsAmountTest);
+        when(statisticsMock.usersNames()).thenReturn(userList);
+        StatisticsMethods statisticsMethodsTest = new StatisticsMethods(statisticsMock);
+
+        //When
         statisticsMethodsTest.calculateAdvStatistics(statisticsMock);
-        List<Integer> wynik = statisticsMethodsTest.showStatistics();
+        Map<Integer, Integer> wynik = statisticsMethodsTest.showStatistics();
 
         //then
-        Assert.assertEquals(Integer.valueOf(100), wynik.get(0));
+        Assert.assertEquals(Integer.valueOf(1000), wynik.get(1));
     }
 
-     */
+    @Test
+    public void testCalculateStatisticsCommentsAmount() {
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+        int postsAmountTest = 1000;
+        int commentsAmountTest = 0;
+        ArrayList<String> userList = new ArrayList<String>();
+        for (int i = 0; i < 100; i++) {userList.add("user");}
+
+        when(statisticsMock.postsCount()).thenReturn(postsAmountTest);
+        when(statisticsMock.commentsCount()).thenReturn(commentsAmountTest);
+        when(statisticsMock.usersNames()).thenReturn(userList);
+        StatisticsMethods statisticsMethodsTest = new StatisticsMethods(statisticsMock);
+
+        //When
+        statisticsMethodsTest.calculateAdvStatistics(statisticsMock);
+        Map<Integer, Integer> wynik = statisticsMethodsTest.showStatistics();
+
+        //then
+        Assert.assertEquals(Integer.valueOf(0), wynik.get(2));
+    }
+
+    @Test
+    public void testCalculateStatisticsPostsPerUsers() {
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+        int postsAmountTest = 1000;
+        int commentsAmountTest = 0;
+        ArrayList<String> userList = new ArrayList<String>();
+        for (int i = 0; i < 100; i++) {userList.add("user");}
+
+        when(statisticsMock.postsCount()).thenReturn(postsAmountTest);
+        when(statisticsMock.commentsCount()).thenReturn(commentsAmountTest);
+        when(statisticsMock.usersNames()).thenReturn(userList);
+        StatisticsMethods statisticsMethodsTest = new StatisticsMethods(statisticsMock);
+
+        //When
+        statisticsMethodsTest.calculateAdvStatistics(statisticsMock);
+        Map<Integer, Integer> wynik = statisticsMethodsTest.showStatistics();
+
+        //then
+        Assert.assertEquals(Integer.valueOf(10), wynik.get(3));
+    }
+
+    @Test
+    public void testCalculateStatisticsCommentsPerUsers() {
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+        int postsAmountTest = 1000;
+        int commentsAmountTest = 0;
+        ArrayList<String> userList = new ArrayList<String>();
+        for (int i = 0; i < 100; i++) {userList.add("user");}
+
+        when(statisticsMock.postsCount()).thenReturn(postsAmountTest);
+        when(statisticsMock.commentsCount()).thenReturn(commentsAmountTest);
+        when(statisticsMock.usersNames()).thenReturn(userList);
+        StatisticsMethods statisticsMethodsTest = new StatisticsMethods(statisticsMock);
+
+        //When
+        statisticsMethodsTest.calculateAdvStatistics(statisticsMock);
+        Map<Integer, Integer> wynik = statisticsMethodsTest.showStatistics();
+
+        //then
+        Assert.assertEquals(Integer.valueOf(0), wynik.get(4));
+    }
+
+    @Test
+    public void testCalculateStatisticsComments0PerPosts1000() {
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+        int postsAmountTest = 1000;
+        int commentsAmountTest = 0;
+        ArrayList<String> userList = new ArrayList<String>();
+        for (int i = 0; i < 100; i++) {userList.add("user");}
+
+        when(statisticsMock.postsCount()).thenReturn(postsAmountTest);
+        when(statisticsMock.commentsCount()).thenReturn(commentsAmountTest);
+        when(statisticsMock.usersNames()).thenReturn(userList);
+        StatisticsMethods statisticsMethodsTest = new StatisticsMethods(statisticsMock);
+
+        //When
+        statisticsMethodsTest.calculateAdvStatistics(statisticsMock);
+        Map<Integer, Integer> wynik = statisticsMethodsTest.showStatistics();
+
+        //then
+        Assert.assertEquals(Integer.valueOf(0), wynik.get(5));
+    }
+
+    @Test(expected = java.lang.ArithmeticException.class)
+    public void testCalculateStatisticsComments1000PerPosts0() {
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+        int postsAmountTest = 0;
+        int commentsAmountTest = 1000;
+        ArrayList<String> userList = new ArrayList<String>();
+        for (int i = 0; i < 100; i++) {userList.add("user");}
+
+        when(statisticsMock.postsCount()).thenReturn(postsAmountTest);
+        when(statisticsMock.commentsCount()).thenReturn(commentsAmountTest);
+        when(statisticsMock.usersNames()).thenReturn(userList);
+        StatisticsMethods statisticsMethodsTest = new StatisticsMethods(statisticsMock);
+
+        //When
+        statisticsMethodsTest.calculateAdvStatistics(statisticsMock);
+        Map<Integer, Integer> wynik = statisticsMethodsTest.showStatistics();
+
+        //then
+        Assert.assertEquals(Integer.valueOf(0), wynik.get(5));
+    }
 
 
 }
