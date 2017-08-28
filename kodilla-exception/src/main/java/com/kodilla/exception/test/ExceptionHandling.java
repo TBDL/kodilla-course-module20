fd7d7653a86e2e70dd93462dbcf0a71a5a2756e2
Tomasz Badelek <tbadelek@gmail.com>
2017-08-29 01:40:13 +0200

    8.3

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -0,0 +1,23 @@
+package com.kodilla.exception.test;
+
+
+import java.time.LocalDateTime;
+
+public class ExceptionHandling extends Exception {
+
+
+    public static void main(String args[]) {
+
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+        try {
+            secondChallenge.probablyIWillThrowException(11.5, 5.5);
+        } catch (Exception e) {
+            System.out.println("Problem on: " + e);
+        } finally {
+            System.out.println("Date and time of operation: " + LocalDateTime.now());
+        }
+
+    }
+
+}
